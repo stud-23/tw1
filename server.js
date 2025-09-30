816609c670a2ba38faa6dfcad3802d9fadafeee3
stud-23 <palangeanuandrei23@stud.ase.ro>
2025-09-30 09:42:38 +0300

    Create server.js
    seminar 1

---
+++ server.js
@@ -0,0 +1,12 @@
+//import http module                                             ctrl+c in terminal pt a da stop la sv
+const http = require("http");
+
+//create server
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.write("Hello from node server ...")
+    res.end();
+})
+
+//listen on port 3000
+server.listen(3000,()=>console.log("Server is running on port 3000..."))
